7dcaeddc370d084dd933992583e55acc828f7efa
HectorPR4546 <h_perez93@yahoo.com>
2025-04-09 15:07:56 -0400

    Task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -15,13 +15,11 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree->left)
 	{
 		binary_tree_delete(tree->left);
-		free(tree->left);
 	}
 
 	if (tree->right)
 	{
 		binary_tree_delete(tree->right);
-		free(tree->right);
 	}
 	free(tree);
 }
