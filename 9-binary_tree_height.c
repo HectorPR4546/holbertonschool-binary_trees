5b87eddcb2c0ad21f224bd196d2547ad954ff59d
HectorPR4546 <h_perez93@yahoo.com>
2025-04-12 15:40:15 -0400

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,15 +7,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_h;
-	size_t right_h;
 
 	if (tree == NULL)
 		return (0);
 
-
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
-
-	return (left_h > right_h ? left_h : right_h + 1);
+	else
+	{
+		size_t left_h = binary_tree_height(tree->left);
+		size_t right_h = binary_tree_height(tree->right);
+		
+		if (left_h < right_h)
+			return (left_h + 1);
+		else
+			return (right_h + 1);
+	}
 }
