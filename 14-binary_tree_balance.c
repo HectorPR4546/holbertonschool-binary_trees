f6683b4b8c44d76e1f54b282668da9150d1165b6
HectorPR4546 <h_perez93@yahoo.com>
2025-04-13 13:23:18 -0400

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,28 +10,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int left_h = 0;
 	int right_h = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	if (tree->left == NULL)
-	{
+	if (!tree->left && !tree->right)
 		return (1);
-	}
-
-	if (tree->right == NULL)
-	{
-		return (1);
-	}
 
 	if (tree->left)
-	{
 		left_h = binary_tree_height(tree->left) + 1;
-	}
 
 	if (tree->right)
-	{
-		right_h = binary_tree_height(tree->right) + 1;
-	}
+	right_h = binary_tree_height(tree->right) + 1;
 
 	return (left_h > right_h ? left_h : right_h);
 }
